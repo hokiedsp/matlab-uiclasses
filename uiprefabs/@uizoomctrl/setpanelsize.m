34779d36ab89d3f5e28a85476e48daa8cfaef673
Kesh Ikuma <tikuma@gmail.com>
2014-09-09 23:19:07 -0500

    Split uizoomctrl -> uizoomctrl + zoompanctrl
    Should've been deleted earlier

--- uiprefabs/@uizoomctrl/setpanelsize.m
+++
@@ -1,6 +0,0 @@
-function setpanelsize(obj) % set uipanel size
-% fix panel size
-u = get(obj.hg,'Units');
-set(obj.hg,'Units','pixels');
-pos = get(obj.hg,'Position');
-set(obj.hg,'Position',[pos([1 2]) obj.panelsize],'Units',u);
